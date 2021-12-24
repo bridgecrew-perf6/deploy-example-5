53bf601606eb75cbce2cb38bb29b927d00ff78e0
humanoid22 <marwaharhad@gmail.com>
2021-12-24 12:22:03 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,4 @@ const app= express()
 const _dirname=path.dirname(fileURLToPath(import.meta.url))
 const publicDirectory=path.join(_dirname,'../public')
 app.use(express.static(publicDirectory))
-const port= 10020
-app.listen(port,()=>{
-    console.log(`server is up on port ${port}`)
-})
+.listen(process.env.PORT || 5000)
